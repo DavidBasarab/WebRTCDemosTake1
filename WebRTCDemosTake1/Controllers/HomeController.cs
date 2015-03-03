bf303cff524f0afbdd5a4016c041ff29a2f88f7f
Devin Rader <devin@twilio.com>
2015-03-03 11:13:08 -0500

    Cleaning up samples

--- WebRTCDemosTake1/Controllers/HomeController.cs
+++ WebRTCDemosTake1/Controllers/HomeController.cs
@@ -12,20 +12,12 @@ namespace WebRTCDemosTake1.Controllers
         // GET: Home
         public ActionResult Caller()
         {
-            ViewBag.IceServers = GetIceServers().IceServers;
             return View();
         }
 
         public ActionResult Callee()
-        {
-            ViewBag.IceServers = GetIceServers().IceServers;
+        {            
             return View();
-        }
-
-        private Token GetIceServers()
-        {
-            TwilioRestClient client = new TwilioRestClient(Credentials.AccountSid, Credentials.AuthToken);
-            return client.CreateToken();
         }
     }
 }
